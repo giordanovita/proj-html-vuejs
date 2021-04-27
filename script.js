d24640198774c553f1a35b39434b383423b86e16
giordano <giordanovitag@libero.it>
2021-04-27 10:53:01 +0200

    caricamento file iniziale

---
+++ script.js
@@ -0,0 +1,28 @@
+function albumCard(){
+    new Vue({
+        el: '#app',
+        data: {
+            
+
+
+                    },
+        mounted:{
+            },
+            
+            
+        
+
+        methods:{
+                    
+
+            
+            }
+        })
+    }
+
+
+function init() {
+    
+}
+document.addEventListener('DOMContentLoaded', init);
+
